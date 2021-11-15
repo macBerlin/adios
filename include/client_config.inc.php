288b592f2a864a74cd59771a869c8d4fef9930df
macBerlin <75625721+macBerlin@users.noreply.github.com>
2021-11-15 19:04:28 +0100

    Create client_config.inc.php

---
+++ include/client_config.inc.php
@@ -0,0 +1,8 @@
+<?PHP
+$APIUSER="############";
+$APIPASS="###########";
+$JAMFPRO="https://jamf.company.com:8443";
+$EA_NAME="eacs_done";
+//You can use everything as AGENT String but keep in mind to also update the shell script.
+$AGENT="SomeRandomString#1";
+?>
